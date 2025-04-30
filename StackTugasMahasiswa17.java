dde2068e597a66e8a71f064141b3ea63e80f8dc8
LAPTOP-RBLOQQKD\Naswa Nida <naswanida3@gmail.com>
2025-04-30 15:11:59 +0700

    percobaan 2

--- StackTugasMahasiswa17.java
+++ StackTugasMahasiswa17.java
@@ -70,4 +70,19 @@ public class StackTugasMahasiswa17 {
     public int jumlahTugas() {
         return top + 1;
     }
+
+    // menambahkan method untuk soal percobaan 2
+    public String konversiDesimalKeBiner(int nilai){
+        StackKonversi17 stack = new StackKonversi17();
+        while(nilai > 0){
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while(!stack.isEmpty()){
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
